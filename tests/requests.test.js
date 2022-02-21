d16efb0bd95c00dd7cb75038477a76a58d75e136
Ashwin Menon <ashtwentyfour@Ashwins-MacBook-Air.local>
2022-02-21 15:33:05 -0500

    added lookup unit test

--- tests/requests.test.js
+++ tests/requests.test.js
@@ -2,21 +2,21 @@ const request = require('supertest');
 const app = require('../app.js');
 
 describe('PUT Endpoints', () => {
-    it('should insert into Bloom Filter', async () => {
-        const res = await request(app)
-            .put('/api/insert')
-            .send({
-                'item': 'testitem',
-            });
-        expect(res.statusCode).toEqual(201);
+  it('should insert into Bloom Filter', async () => {
+    const res = await request(app)
+        .put('/api/insert')
+        .send({
+          'item': 'testitem',
+        });
+    expect(res.statusCode).toEqual(201);
   });
 });
 
 describe('GET Endpoints', () => {
-    it('should lookup an item in Bloom Filter', async () => {
-        const res = await request(app)
-            .get('/api/testitem');
-        expect(res.statusCode).toEqual(200);
-        expect(res.body.body.message).toBe('testitem is probably present');
-    });
+  it('should lookup an item in Bloom Filter', async () => {
+    const res = await request(app)
+        .get('/api/testitem');
+    expect(res.statusCode).toEqual(200);
+    expect(res.body.body.message).toBe('testitem is probably present');
+  });
 });
